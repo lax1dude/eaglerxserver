e65d5569ea989fb92b4dcad16095766897f72424
lax1dude <cgiacun@gmail.com>
2025-05-29 16:25:49 -0700

    Bump versions

--- core/src/main/java/net/lax1dude/eaglercraft/backend/server/base/EaglerXServerVersion.java
+++ core/src/main/java/net/lax1dude/eaglercraft/backend/server/base/EaglerXServerVersion.java
@@ -22,7 +22,7 @@ public class EaglerXServerVersion {
 
 	public static final String BRAND = "EaglercraftXServer";
 	public static final String AUTHOR = "lax1dude";
-	public static final String VERSION = "1.0.6";
+	public static final String VERSION = "1.0.7";
 
 	public static final SemanticVersion UPDATE_VERSION = SemanticVersion.parse(VERSION);
 
